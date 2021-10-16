d5f2698ba5ad245b32a0402f979fac74f015d41f
Roni Hytonen <roni.hytonen93@gmail.com>
2021-10-16 22:15:52 +0300

    Add comments to the blogs backend.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,18 +1,32 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs=await Blog
-    .find({}).populate('user', {name: 1, username: 1});
+  const blogs = await Blog
+    .find({})
+    .populate('user', {name: 1, username: 1})
+    .populate('comments', {text: 1});
   res.json(blogs.map(blog => blog.toJSON()));
 });
 
 blogsRouter.get('/:id', async (req, res) => {
   const blog = await Blog.findById(req.params.id);
   blog ? res.json(blog.toJSON()) : res.status(404).end();
+});
+
+blogsRouter.get('/:id/comments', async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+    .populate('comments', {text: 1});
+  console.log(blog);
+  if (!blog) {
+    res.status(404).end();
+  }
+
+  res.json(blog.comments.map(comment => comment.toJSON()));
 });
 
 blogsRouter.post('/', async (req, res) => {
@@ -56,6 +70,44 @@ blogsRouter.post('/', async (req, res) => {
   await user.save();
 
   res.json(savedBlog.toJSON());
+});
+
+blogsRouter.post('/:id/comments', async (req, res) => {
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+  if (!req.token || !decodedToken.id) {
+    return res.status(401).json({error: 'token missing or invalid'});
+  }
+
+  console.log(req);
+
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    res.status(404).end();
+  }
+
+  console.log(blog);
+
+  const body = req.body;
+  if (!body.text) {
+    return res.status(400).json({
+      error: 'Body content `text` missing'
+    });
+  }
+
+  console.log(body);
+
+  const comment = new Comment({
+    text: body.text,
+    blog: blog._id
+  });
+
+  console.log(comment);
+
+  const savedComment = await comment.save();
+  blog.comments = blog.comments.concat(savedComment._id);
+  await blog.save();
+
+  res.json(savedComment.toJSON());
 });
 
 blogsRouter.delete('/:id', async (req, res) => {
